52463f1d8d11b148cb414a5ef4787a787fcd3fc7
quinleys <quinleys@student.arteveldehs.be>
2017-10-05 10:28:54 +0200

    Oefening 1

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+    <?php
+    $naam = 'Quinten Leysen';
+    $string1 = 'Hello';
+    $string2 = 'World';
+    $x = 10;
+    $y = 7;  
+    ?>
+    
 <!DOCTYPE html>
 <html lang="nl">
 <head>
@@ -5,22 +13,12 @@
     <title>Oefening 1</title>
 </head>
 <body>
-    <?php
-    // Taak 1
-    $naam = "Quinten Leysen";
-    echo "Welkom" + $naam;
-    // Taak 2
-    $string1 = "Hello";
-    $string2 = "World";
-    echo $string1 + " " + $string2;
-    // Taak 3
-    $x = 10;
-    $y + 7;
-    echo $x + $y;
-    echo $x - $y;
-    echo $x * $y;
-    echo $x / $y;
-    echo $x % $x;
-    ?>
+<h1><?php echo 'Welkom ' . $naam; ?> </h1>
+<p><?php echo $string1 . ' ' . $string2; ?> </p>
+<p> <?php echo $x - $y ?> </p>
+<p> <?php echo $x + $y ?> </p>
+<p> <?php echo $x * $y ?> </p>
+<p> <?php echo $x / $y ?> </p>
+<p> <?php echo $x % $y ?> </p>
 </body>
 </html>
