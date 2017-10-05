bbaea42f70f14d54d043da6cb65cb344cf7bf6c4
quinleys <quinleys@student.arteveldehs.be>
2017-10-05 09:37:40 +0200

    Oefening 1

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Taak 1</title>
+</head>
+<body>
+    <?php
+
+    // Taak 1
+    $naam = "Quinten Leysen";
+    echo $naam;
+    // Taak 2
+    $string1 = "Hello";
+    $string2 = "World";
+    echo $string1 + " " + $string2;
+    // Taak 3
+    $x = 10;
+    $y + 7;
+    echo $x + $y;
+    echo $x - $y;
+    echo $x * $y;
+    echo $x / $y;
+    echo $x % $x;
+
+    ?>
+</body>
+</html>
